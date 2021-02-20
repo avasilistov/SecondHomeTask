25d941818ff3be6621ad94ad5fb9922621c2295d
avasilistov <kvoroncova@gmail.com>
2021-02-20 20:15:11 +0200

    В тулбаре этого активити справа разместить выпадающее меню из двух пунктов. Один из которых “About”, вызывает диалоговое окно (DialogFragment), в котором вы можете написать кем создано это приложение и поле ввода с кнопкой “Send”. Введя какой-либо текст и нажав на кнопку, диалог закрывается, а на активити появляется Toast c текстом что вы вводили. (для передачи использовать callback).

--- app/src/main/java/com/demo/secondhometask/fragment/AboutFragment.java
+++ app/src/main/java/com/demo/secondhometask/fragment/AboutFragment.java
@@ -6,14 +6,10 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-
 import com.demo.secondhometask.MainActivity;
 import com.demo.secondhometask.R;
 
